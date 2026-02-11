7887ec75805556a8658ae6e495ea59c5306894de
Mario Morales <mariomorales1025@gmail.com>
2026-02-10 19:17:36 -0500

    feat: Add soft delete and audit fields to user model

--- app/infrastructure/db/models/base_model.py
+++ app/infrastructure/db/models/base_model.py
@@ -1,7 +1,35 @@
 """Base model for SQLAlchemy ORM."""
 
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
+from sqlalchemy.types import Uuid
+from sqlalchemy_easy_softdelete.mixin import generate_soft_delete_mixin_class  # pylint: disable=E0401
+
+SOFT_DELETE_MIXIN_CLASS: type = generate_soft_delete_mixin_class()
 
 
-class Base(DeclarativeBase):
+class UUIDModel:
+    """Base UUID class for SQLAlchemy models."""
+
+    uuid: Mapped[Uuid[str]] = mapped_column(Uuid(as_uuid=True), default=func.uuid())
+
+
+class Base(DeclarativeBase, UUIDModel):
     """Base class for SQLAlchemy models."""
+
+
+class AuditableModel:
+    """Base Auditable class for SQLAlchemy models."""
+
+    created_at: Mapped[datetime] = mapped_column(default=func.now, comment="Fecha de creación")
+    updated_at: Mapped[datetime] = mapped_column(onupdate=func.now, comment="Fecha de actualización")
+    created_by: Mapped[int | None] = mapped_column(comment="ID del usuario que creó el registro")
+    updated_by: Mapped[int | None] = mapped_column(comment="ID del usuario que actualizó el registro")
+
+
+class SoftDeleteMixin(SOFT_DELETE_MIXIN_CLASS):  # type: ignore[misc]
+    """Mixin for soft delete functionality."""
+
+    deleted_at: datetime
--- app/infrastructure/db/models/user.py
+++ app/infrastructure/db/models/user.py
@@ -1,13 +1,12 @@
 """Model representing a user."""
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.sql import func
-from sqlalchemy.types import Enum, Uuid
+from sqlalchemy.types import Enum
 
-from app.infrastructure.db.models.base_model import Base
+from app.infrastructure.db.models.base_model import AuditableModel, Base, SoftDeleteMixin
 
 
-class User(Base):
+class User(Base, SoftDeleteMixin, AuditableModel):
     """Model representing a user."""
 
     __tablename__ = "users"
@@ -18,7 +17,6 @@ class User(Base):
     first_surname: Mapped[str] = mapped_column(String(100), nullable=False, comment="Primer apellido del usuario")
     second_surname: Mapped[str | None] = mapped_column(String(100), comment="Segundo apellido del usuario")
     gender: Mapped[Enum] = mapped_column(Enum("male", "female", "other"), nullable=False)
-    uuid: Mapped[Uuid[str]] = mapped_column(Uuid(as_uuid=True), server_default=func.uuid())
 
     def __repr__(self) -> str:
         """Return a string representation of the user."""
